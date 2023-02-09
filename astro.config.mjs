27ef76507046f6b7b5c92513e52e550e33182375
AngelNext <contact@angelnext.dev>
2023-02-10 00:14:20 +0100

    Added image optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import prefetch from '@astrojs/prefetch';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
+import image from "@astrojs/image";
 
 export default defineConfig({
-	site: 'https://angelnext.dev/',
-	integrations: [sitemap(), prefetch()],
+	site: "https://angelnext.dev/",
+	integrations: [
+		sitemap(),
+		prefetch(),
+		image({
+			serviceEntryPoint: "@astrojs/image/sharp",
+		}),
+	],
 });
