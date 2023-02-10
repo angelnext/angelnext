ef8083f738f6034f2d2eb8ac64809ad935c011c8
AngelNext <contact@angelnext.dev>
2023-02-10 18:26:53 +0100

    Added Manifest

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 import image from "@astrojs/image";
+import AstroPWA from "@vite-pwa/astro";
 
 export default defineConfig({
 	site: "https://angelnext.dev/",
@@ -10,6 +11,30 @@ export default defineConfig({
 		prefetch(),
 		image({
 			serviceEntryPoint: "@astrojs/image/sharp",
+		}),
+		AstroPWA({
+			registerType: "autoUpdate",
+			injectRegister: "script",
+			workbox: {
+				navigateFallback: "/404",
+				globPatterns: ["**/*.{js,css,html,webp,png,svg}"],
+			},
+			manifestFilename: "app.webmanifest",
+			manifest: {
+				name: "AngelNext",
+				short_name: "AngelNext",
+				background_color: "#fff",
+				start_url: ".",
+				display: "standalone",
+				description: "The personal site of AngelNext.",
+				icons: [
+					{
+						src: "/favicon.svg",
+						sizes: "512x512",
+						type: "image/svg+xml",
+					},
+				],
+			},
 		}),
 	],
 });
