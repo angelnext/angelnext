d62c1ef58032964c41c14825cde09f71f4c12446
Ángel González Vázquez <contact@angelnext.dev>
2023-02-12 03:03:59 +0000

    Cleared repo

--- src/env.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../.astro/types.d.ts" />
-/// <reference types="@astrojs/image/client" />
-/// <reference types="vite-plugin-pwa/client" />
-/// <reference types="@vite-pwa/astro" />
-
-declare module "virtual:pwa-info";
